755efacbbd8d6cbdfc4bdfdc1516e20eb8f59c97
gakisan8273 <mint.daa.a2@gmail.com>
2022-10-28 09:37:34 +0900

    55ページ

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // Copyright 2018 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,14 +13,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      title: 'Translate app',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Translate App'),
+      ),
+      body: Column(
+        // 縦方向の位置
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: TextField(
+              maxLines: 5,
+              decoration: InputDecoration(
+                hintText: '文章を入力してください',
+              ),
+            ),
+          ),
+
+          // 余白を作るbox
+          const SizedBox(height: 20),
+          ElevatedButton(onPressed: () {}, child: const Text('変換')),
+        ],
       ),
     );
   }
